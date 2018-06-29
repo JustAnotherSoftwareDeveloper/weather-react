64ce9aa5340ced3102b377566ab2813b3a1cba2d
Michael <michael.sila.jobs@gmail.com>
2018-06-28 20:35:52 -0600

    Got PDF for about page to work, need to alter the styling

--- src/about-page/about.jsx
+++ src/about-page/about.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
+import resume from './MichaelSila_Resume.pdf';
+import './about.less';
+
 class About extends Component {
     state = {
         numPages: null,
         pageNumber: 1,
     }
 
+
     onDocumentLoad = ({ numPages }) => { this.setState({ numPages }) };
     render() {
         return (
-            <Document file="MichaelSila_Resume.pdf" onLoadSuccess={this.onDocumentLoad}>
-                <Page pageNumber={this.state.pageNumber} />
-            </Document>
+            <div>
+                <Document file={resume} onLoadSuccess={this.onDocumentLoad}>
+                    {
+                        Array.from(new Array(this.state.numPages), (el, index) =>
+                         <Page key={`page_${index + 1}`} renderTextLayer={false} renderAnnotations={false}
+                            pageNumber={index + 1}></Page>)
+                    }
+                </Document>
+            </div>
+
         );
     }
 }
